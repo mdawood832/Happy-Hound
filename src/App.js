5113ef62b511291d1b1ec8eb295a8f0010b414e0
knorth <kayci.north@gmail.com>
2022-09-24 15:37:42 -0600

    setup baseURL configs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import './App.css';
 import Nav from './components/Nav'
 import Landing from './components/Landing';
 
+//add baseURL outside of APP
+let baseURL = ''
+
+if(process.env.NODE_ENV === 'developement') {  //NODE_ENV specifically is used to state whether a particular environment is a production or a development environment
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'your heroku bakend url here'
+}
+console.log('current base URL:', baseURL)
+
+
 function App() {
   return (
     <div className="App">
