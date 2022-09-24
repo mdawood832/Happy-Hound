73cd8a2755bfd1ecde6336140689ed9be14c7a65
mdawood832 <mdawood832@gmail.com>
2022-09-24 09:36:45 -0500

    going to main

---
+++ src/components/Nav.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import HappyHoundLogo from '../assets/hh (1).png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Nav = () => {
+    return(
+        <nav>
+            <div className='navContainer'>
+                <div className='Logo'>
+                <a href="">
+                    <img className='logo' src={HappyHoundLogo} alt="" />
+                </a>
+                </div>
+
+                <ul className='navLinks'>
+                    <li className='navList'>
+                        <a href="/" className='navLink'>
+                            Home
+                        </a>
+                    </li>
+
+                    <li className='navList'>
+                        <a href="/" className='navLink'>
+                            Shop
+                        </a>
+                    </li>
+
+                    <li className='navList'>
+                        <a href="/" className='navLink'>
+                            Our Story
+                        </a>
+                    </li>
+
+                    <li className='navList'>
+                        <a href="/" className='navLink'>
+                        <FontAwesomeIcon icon="cart-shopping" />
+                        </a>
+                        <span className='cart__length'>2</span>
+                    </li>
+
+                    <li className='navList'>
+                        <a href="/" className='navLink'>
+                            Sign In/Sign Out
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
