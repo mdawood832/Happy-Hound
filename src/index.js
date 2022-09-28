aedeafca996a8c309c928817b874432ab8fb888e
Eric-Peltzman33 <erpeltz@gmail.com>
2022-09-28 19:25:15 -0400

    trying to render the login and register pages

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+import {render} from 'react-dom'; 
+import Register from './components/Register';
+import Login from './components/Login';
 
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
- 
- 
- 
- 
- 
-
-
 library.add(faShoppingCart)
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
