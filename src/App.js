ccda3237a3809937ac93f658c18caf5e6b95491c
mdawood832 <mdawood832@gmail.com>
2022-09-28 19:36:16 -0500

    base URL

--- src/App.js
+++ src/App.js
@@ -6,11 +6,30 @@ import Featured from './components/Featured';
 import ExploreMore from './components/ExploreMore';
 import Footer from './components/Footer';
 
+// add base URl 
+let baseURL = ''
+if (process.env.NODE_ENV === 'development' ){
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'heroku backend uRL '
+}
 
-
+console.log('current base URL' , baseURL)
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+
+
+
+
   render() {
     return (
       <div className='App'>
