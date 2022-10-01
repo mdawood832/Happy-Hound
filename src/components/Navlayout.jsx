07075913296f80acc997686727505bb0df69d4f5
Eric-Peltzman33 <erpeltz@gmail.com>
2022-10-01 13:06:04 -0400

    commit

---
+++ src/components/Navlayout.jsx
@@ -0,0 +1,15 @@
+ import { Link } from 'react-router-dom';
+
+ 
+export function Nav() {
+    return (
+    <>
+      
+            <Link to="/login">Login</Link>
+            <br />
+            <Link to="/register">Register</Link>
+            <br />
+        </>
+    );
+        
+} 
