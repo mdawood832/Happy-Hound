b07bd6f13e6d90e2e62b3a6ad8182a176354231c
Eric-Peltzman33 <erpeltz@gmail.com>
2022-10-01 12:01:31 -0400

    rendered a simple nav

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom'; 
+ import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom'; 
 import Login from './components/Login';
 import Register from './components/Register';
 import './App.css';
-// import Nav from './components/Nav';
+ import Nav from './components/Nav';
 
 import Landing from './components/Landing';
 
-class App extends Component {
-   
-  render() {
-    return (
-      <div className="App">
-      <Routes>
-       
-      <Route path="/" element={<Landing />} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        
-         
-      </Routes>
-      </div>
-       
-         
-        
-    );
-  }
-}
 
-export default App;
+function App() {
+
+    return (
+      <>
+      <nav>
+       <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      </>
+    );
+    }
   
+  export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import Register from './components/Register';
-import Login from './components/Login';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 library.add(faShoppingCart)
@@ -13,11 +12,7 @@ library.add(faShoppingCart)
 
 ReactDOM.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
+  <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
