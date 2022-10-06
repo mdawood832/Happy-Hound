503a710848d240be38093577f40d3043551f8200
Eric-Peltzman33 <erpeltz@gmail.com>
2022-10-05 22:19:07 -0400

    Revert "Revert "Merge pull request #21 from mdawood832/Login/register_2""
    This reverts commit b3f6c44f9264894fe517d5571b89ae7d3716e17b.

---
+++ src/pages/Products.jsx
@@ -0,0 +1,43 @@
+import "../css/Products.css";
+import Features from "../components/ui/Features";
+import React, { Component } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+
+
+class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     products: []
+    };
+  }
+
+
+  render() {
+    console.log(this.props)
+    return (
+      <div className="App">
+        <h1>Products</h1>
+        
+        {this.props.products.map((product, index) => {
+          return (
+              <div className="productsContainer" key={index}>
+                      <Link to="/edit/:id">
+                        <h2 className="sectionTitle">{product.name}</h2>
+                      </Link>
+
+                      <Link to={'/detail/'+ product._id}>
+                        <img src={product.imgURL} />
+                      </Link>
+              </div>
+          );
+        })}
+
+
+
+      </div>
+    );
+  }
+}
+
+export default Products
